5fc13728abc3f883afe3bbeffd50eee8423e79b2
ksykes <kaitlyn.sykes@me.com>
2019-02-26 20:34:43 -0500

    Updates

---
+++ precache-manifest.435ae79a6ecbfb9b0bbf8fd74f6b7804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57424e58fa80d4b30c18cd0fc27d68ce",
+    "url": "/bd-wedding/static/media/hero.57424e58.jpg"
+  },
+  {
+    "revision": "18de81c54fc210fb90ab",
+    "url": "/bd-wedding/static/js/runtime~main.18de81c5.js"
+  },
+  {
+    "revision": "1aaa5c7320768d48f8d9",
+    "url": "/bd-wedding/static/js/main.1aaa5c73.chunk.js"
+  },
+  {
+    "revision": "828f3f33266d11a32de1",
+    "url": "/bd-wedding/static/js/1.828f3f33.chunk.js"
+  },
+  {
+    "revision": "1aaa5c7320768d48f8d9",
+    "url": "/bd-wedding/static/css/main.b15ac66c.chunk.css"
+  },
+  {
+    "revision": "d8e9e65c02443452921c1bedbfe4c0cb",
+    "url": "/bd-wedding/index.html"
+  }
+];
