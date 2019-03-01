66b655153279c052ad5e8d4f4d1921325bbb0840
ksykes <kaitlyn.sykes@me.com>
2019-03-01 10:06:15 -0500

    fixed form labels

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -32,41 +32,65 @@ class RSVP extends Component {
             <section className="rsvpForm">
                 <h2>Please RSVP by April 1, 2015.</h2>
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="name">Your full name:</label>
-                    <input type="text" name="name" value={name} onChange={this.handleChange} />
-                    <label htmlFor="email">Your email address</label>
-                    <input type="email" name="email" value={email} onChange={this.handleChange} />
-                    <label htmlFor="address">Your mailing address</label>
-                    <textarea name="address" value={address} onChange={this.handleChange}></textarea>
+                    <label htmlFor="name">
+                        <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        Your full name
+                    </label>
+                    <label htmlFor="email">
+                        <input type="email" name="email" value={email} onChange={this.handleChange} />
+                        Your email address
+                    </label>
+                    <label htmlFor="address">
+                        <textarea name="address" value={address} onChange={this.handleChange}></textarea>
+                        Your mailing address
+                    </label>
                     <fieldset>
                         <legend>RSVP:</legend>
-                        <input type="radio" id="yes" name="rsvp" value="Yes" onChange={this.handleChange} />
-                        <label htmlFor="yes">Accepts with pleasure</label>
-                        <input type="radio" id="no" name="rsvp" value="No" onChange={this.handleChange} />
-                        <label htmlFor="no">Denies with regret</label>
+                        <label htmlFor="yes">
+                            <input type="radio" id="yes" name="rsvp" value="Yes" onChange={this.handleChange} />
+                            Accepts with pleasure
+                        </label>
+                        <label htmlFor="no">
+                            <input type="radio" id="no" name="rsvp" value="No" onChange={this.handleChange} />
+                            Denies with regret
+                        </label>
                     </fieldset>
                     <fieldset>
                         <legend>Dinner choice:</legend>
-                        <input type="radio" id="one" name="dinner" value="Option 1" onChange={this.handleChange} />
-                        <label htmlFor="one">Option 1</label>
-                        <input type="radio" id="two" name="dinner" value="Option 2" onChange={this.handleChange} />
-                        <label htmlFor="two">Option 2</label>
-                        <input type="radio" id="three" name="dinner" value="Option 3" onChange={this.handleChange} />
-                        <label htmlFor="three">Option 3</label>
+                        <label htmlFor="one">
+                            <input type="radio" id="one" name="dinner" value="Option 1" onChange={this.handleChange} />
+                            Option 1
+                        </label>
+                        <label htmlFor="two">
+                            <input type="radio" id="two" name="dinner" value="Option 2" onChange={this.handleChange} />
+                            Option 2
+                        </label>
+                        <label htmlFor="three">
+                            <input type="radio" id="three" name="dinner" value="Option 3" onChange={this.handleChange} />
+                            Option 3
+                        </label>
                     </fieldset>
                     <hr/>
                     <p>If you don't have a plus one, leave this section blank...</p>
                     <legend>Plus One</legend>
-                    <input type="text" name="plusOneName" value={plusOneName} onChange={this.handleChange} />
-                    <label htmlFor="plusOneName">Name</label>
+                    <label htmlFor="plusOneName">
+                        <input type="text" name="plusOneName" value={plusOneName} onChange={this.handleChange} />
+                        Your plus one's name
+                    </label>
                     <fieldset>
                         <legend>Your plus one's dinner choice:</legend>
-                        <input type="radio" id="one" name="plusOneDinner" value="Option 1" onChange={this.handleChange} />
-                        <label htmlFor="one">Option 1</label>
-                        <input type="radio" id="two" name="plusOneDinner" value="Option 2" onChange={this.handleChange} />
-                        <label htmlFor="two">Option 2</label>
-                        <input type="radio" id="three" name="plusOneDinner" value="Option 3" onChange={this.handleChange} />
-                        <label htmlFor="three">Option 3</label>
+                        <label htmlFor="one">
+                            <input type="radio" id="one" name="plusOneDinner" value="Option 1" onChange={this.handleChange} />
+                            Option 1
+                        </label>
+                        <label htmlFor="two">
+                            <input type="radio" id="two" name="plusOneDinner" value="Option 2" onChange={this.handleChange} />
+                            Option 2
+                        </label>
+                        <label htmlFor="three">
+                            <input type="radio" id="three" name="plusOneDinner" value="Option 3" onChange={this.handleChange} />
+                            Option 3
+                        </label>
                     </fieldset>
                     <button type="submit">Submit</button>
                 </form>
