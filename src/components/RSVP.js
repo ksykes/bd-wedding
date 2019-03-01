55db393232700be241bff76021dd7bf032eea63b
ksykes <kaitlyn.sykes@me.com>
2019-02-28 22:07:56 -0500

    correction to form

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -16,7 +16,7 @@ class RSVP extends Component {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...this.state })
+            body: encode({ "rsvpForm": "contact", ...this.state })
         })
             .then(() => alert("Success!"))
             .catch(error => alert(error));
