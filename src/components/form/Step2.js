6416ca951b340457e1f552c1ba791ecadaf90868
Kait Sykes <ksykes@rom.on.ca>
2019-05-24 11:48:17 -0400

    fixed plus one dinner

--- src/components/form/Step2.js
+++ src/components/form/Step2.js
@@ -14,25 +14,25 @@ class Step2 extends Component {
                     <legend>{this.props.plusOneName}'s dinner choice:</legend>
                     <div className="dinnerChoices">
                         <label htmlFor="dinnerOne">
-                            <input type="radio" id="dinnerOne" name="dinner" value="beef" onChange={this.props.handleChange} />
+                            <input type="radio" id="dinnerOne" name="plusOneDinner" value="beef" onChange={this.props.handleChange} />
                             AAA Prime Rib Roast <span class="mice">(Gluten Free)</span>
                         </label>
                     </div>
                     <div className="dinnerChoices">
                         <label htmlFor="dinnerTwo">
-                            <input type="radio" id="dinnerTwo" name="dinner" value="salmon" onChange={this.props.handleChange} />
+                            <input type="radio" id="dinnerTwo" name="plusOneDinner" value="salmon" onChange={this.props.handleChange} />
                             Kortright Maple Glaze Salmon <span class="mice">(Gluten Free)</span>
                         </label>
                     </div>
                     <div className="dinnerChoices">
                         <label htmlFor="dinnerThree">
-                            <input type="radio" id="dinnerThree" name="dinner" value="chicken" onChange={this.props.handleChange} />
+                            <input type="radio" id="dinnerThree" name="plusOneDinner" value="chicken" onChange={this.props.handleChange} />
                             Herb Roasted Chicken with Wild Mushroom and Marsala Sauce <span class="mice">(Gluten Free)</span>
                         </label>
                     </div>
                     <div className="dinnerChoices">
                         <label htmlFor="dinnerFour">
-                            <input type="radio" id="dinnerFour" name="dinner" value="vegetarian" onChange={this.props.handleChange} />
+                            <input type="radio" id="dinnerFour" name="plusOneDinner" value="vegetarian" onChange={this.props.handleChange} />
                             Eggplant Parmesan <span class="mice">(Vegetarian/Vegan)</span>
                         </label>
                     </div>
